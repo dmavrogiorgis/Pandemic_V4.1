2dc1c4a69c2ad5feed68b0d273eb4c4a4d5f7af1
Dimitris Mavrogiorgis <jim.mavro@yahoo.gr>
2020-12-26 23:47:50 +0200

    pandemic

--- src/PLH512/client/Node.java
+++ src/PLH512/client/Node.java
@@ -4,24 +4,36 @@ import PLH512.server.*;
 public class Node {
     private double evaluation;
     private Board board;
+    private Node parentNode;
 
-    public Node(double evaluation, Board board){
+    public Node(double evaluation, Board board, Node parentNode){
         this.evaluation = evaluation;
         this.board = board;
+        this.parentNode = parentNode;
     }
 
+    /* GETTERS AND SETTERS */
     public Board getBoard(){
         return board;
     }
     
-    public void setBoard(Board br){
-        this.board = br;
+    public void setBoard(Board board){
+        this.board = board;
     }
 
     public double getEvaluation(){
         return evaluation;
     }
-    public void setEvaluation(double eva){
-        this.evaluation = eva;
+    
+    public void setEvaluation(double evaluation){
+        this.evaluation = evaluation;
+    }
+
+    public Node getParentNode(){
+        return parentNode;
+    }
+    
+    public void setBoard(Node parentNode){
+        this.parentNode = parentNode;
     }
 }
